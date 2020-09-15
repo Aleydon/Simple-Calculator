c390aa8c6b98f88f4efc726f6723bd01035617eb
Aleydon <aleydonroberto@gmail.com>
2020-09-15 09:36:16 -0300

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+		<div className="container">
+			<div className="jumbotron">
+				<h1>CALCULADORA</h1>
+			</div>
+
+			<hr />
+
+			<div className="row">
+				<div className="col-md-4">
+					<input id="numberOne" type="text" className="form-control" placeholder="Digite um número" />
+				</div>
+
+				<div className="col-md-4">
+					<select className="form-control" id="aritmetica">
+						<option value="">--Selecione uma operação</option>
+						<option value="1">Subtração</option>
+						<option value="2">Adição</option>
+						<option value="3">Multiplicação</option>
+						<option value="4">Divisão</option>
+					</select>
+				</div>
+
+				<div className="col-md-4">
+					<input id="numberTwo" type="text" class="form-control" placeholder="Digite um número"/>
+				</div>
+			</div>
+
+			<hr />
+
+			<div className="row">
+				<div className="col-md-4"></div>
+
+				<div className="col-md-4"></div>
+
+				<div className="col-md-4">
+					<button type="button" class="btn btn-lg btn-primary pull-right" onclick="calcular()">Calcular</button>
+				</div>
+			</div>
+
+			<hr />
+
+			<div className="well">
+				Resultado: <input id="result" type="text" class="form-control" readonly/>
+			</div>
+		</div>
+    </>
   );
 }
 
